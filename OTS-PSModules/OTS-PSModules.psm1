463a64d84bf5b368c2d8719acf7f63eb5c74952e
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-03 22:57:46 +1000

    Changed script path, work now?

--- OTS-PSModules/OTS-PSModules.psm1
+++ OTS-PSModules/OTS-PSModules.psm1
@@ -1,4 +1,5 @@
-foreach ($script in (Get-ChildItem -Path ".\" -Depth 2 | ? {$_.FullName.Contains(".ps1")})){
+foreach ($script in (Get-ChildItem -Path $PSScriptRoot -Depth 3 | ? {$_.FullName.Contains(".ps1")})){
+    Write-Output "Export $($script.FullName) please?"
     . $script.FullName
 }
 Export-ModuleMember -Function OTS-OfficeRepair
