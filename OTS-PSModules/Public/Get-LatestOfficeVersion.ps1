201c9f3f46ea7294a3f35c57b11b222fde2e5b2f
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-04 09:48:54 +1000

    get verios public

--- OTS-PSModules/Public/Get-LatestOfficeVersion.ps1
+++ OTS-PSModules/Public/Get-LatestOfficeVersion.ps1
@@ -17,7 +17,17 @@ function Get-LatestOfficeVersion {
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c extrac32.exe /Y /E /L $($catDir) $($catDir)releasehistory.cab" | Out-Null
 
     $process = Get-Process extrac32 -ErrorAction SilentlyContinue -ErrorVariable err
-    if ($err -ne $null) {throw "Office Click-to-Run process didn't seem to run."}
+    if ($ev -ne $null) {
+        [double]$timer = 0
+        while ($err -ne $null){
+            Write-Output "Wait..."
+            $err = $null
+            start-sleep -Seconds 0.05
+            $timer += 0.2
+            $process = Get-Process extrac32 -ErrorAction SilentlyContinue -ErrorVariable err
+            if ($timer -gt 3) {throw "Did not see file extraction run"}
+        }
+    }
     Wait-Process -Id $process.Id
 
     $catalog = Get-Content -Path "$($catDir)releasehistory.xml" -Raw
