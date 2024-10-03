a705e2874f1582d6f29bfe6dda8cb65de945afaa
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-03 22:53:05 +1000

    MORE DEBUG TEXT!!

--- OTS-PSModules/OTS-PSModules.psm1
+++ OTS-PSModules/OTS-PSModules.psm1
@@ -1,4 +1,5 @@
 foreach ($script in (Get-ChildItem -Path $PSScriptRoot -Depth 2 | ? {$_.FullName.Contains(".ps1")})){
+    Write-Output "Export $($script.FullName) please?"
     . $script.FullName
 }
 Export-ModuleMember -Function OTS-OfficeRepair
