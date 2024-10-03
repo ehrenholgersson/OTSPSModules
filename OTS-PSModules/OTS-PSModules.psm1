c5114ed565dbd4218beec73d49dbcc7d684bbb2b
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-03 14:02:48 +1000

    fixed bug

--- OTS-PSModules/OTS-PSModules.psm1
+++ OTS-PSModules/OTS-PSModules.psm1
@@ -1,3 +1,4 @@
 foreach ($script in (Get-ChildItem -Path $PSScriptRoot -Depth 2 | ? {$_.FullName.Contains(".ps1")})){
-    . $_.FullName
+    Write-Output "Loading $script"
+    . $script.FullName
 }
