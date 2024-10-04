f4df964b1a0ac6f275370b824ad008ca022550dd
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-04 10:38:27 +1000

    get version TS

--- OTS-PSModules/Public/Get-LatestOfficeVersion.ps1
+++ OTS-PSModules/Public/Get-LatestOfficeVersion.ps1
@@ -3,32 +3,34 @@ function Get-LatestOfficeVersion {
     param([Parameter(Mandatory,ValueFromPipeline)][string]$channel)
     Write-Output "Download Catalog..."
     $catalogUrl = "https://officecdn.microsoft.com/pr/wsus/releasehistory.cab"
-    $catDir = $env:ProgramData +"\OTS\Catalogs\"
+    $catDir = $env:ProgramData +"\OTS\Catalogs"
 
     If(!(test-path -PathType container $catDir))
     {
         New-Item -ItemType Directory -Path $catDir
     }
 
-    Invoke-WebRequest $catalogUrl -OutFile "$($catDir)releasehistory.cab"
+    Invoke-WebRequest $catalogUrl -OutFile "$($catDir)\releasehistory.cab"
 
      Write-Output "Extract..."
 
-    Start-Process -FilePath "cmd.exe" -ArgumentList "/c extrac32.exe /Y /E /L $($catDir) $($catDir)releasehistory.cab" | Out-Null
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c extrac32.exe /Y /E /L $($catDir) $($catDir)\releasehistory.cab" | Out-Null
+    Start-Sleep -Seconds 0.5 #give time to extract
+    if (!(!(test-path "$catDir\releasehistory.xml")))
 
-    $process = Get-Process extrac32 -ErrorAction SilentlyContinue -ErrorVariable err
-    if ($err -ne $null) {
-        [double]$timer = 0
-        while ($err -ne $null){
-            Write-Output "Wait..."
-            $err = $null
-            start-sleep -Seconds 0.05
-            $timer += 0.05
-            $process = Get-Process extrac32 -ErrorAction SilentlyContinue -ErrorVariable err
-            if ($timer -gt 3) {throw "Did not see file extraction run"}
-        }
-    }
-    Wait-Process -Id $process.Id
+    # $process = Get-Process extrac32 -ErrorAction SilentlyContinue -ErrorVariable err
+    # # if ($err -ne $null) {
+    # #     [double]$timer = 0
+    # #     while ($err -ne $null){
+    # #         Write-Output "Wait..."
+    # #         $err = $null
+    # #         start-sleep -Seconds 0.05
+    # #         $timer += 0.05
+    # #         $process = Get-Process extrac32 -ErrorAction SilentlyContinue -ErrorVariable err
+    # #         if ($timer -gt 3) {throw "Did not see file extraction run"}
+    # #     }
+    # # }
+    # # slee -Id $process.Id
 
     $catalog = Get-Content -Path "$($catDir)releasehistory.xml" -Raw
 
