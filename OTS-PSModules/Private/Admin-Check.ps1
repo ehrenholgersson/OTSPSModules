704fb7de8cf699e951f16d771bb0d57de5235086
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-09 20:44:34 +1100

    hopefully now should run with admin credentials

--- OTS-PSModules/Private/Admin-Check.ps1
+++ OTS-PSModules/Private/Admin-Check.ps1
@@ -2,15 +2,19 @@ function Admin-Check{
     param(
         [string]$ToRun
     )
-
-    $isAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-        $wshell = New-Object -ComObject Wscript.Shell
-    if (!($isAdmin)){
-        $Credential = New-Object System.Management.Automation.PSCredential "$((Get-ComputerInfo).CsDNSHostName)\Maintenence", (Get-SecCredentials "$env:ProgramData\OTS\data\1.dat")
-        if (!($Credential.UserName.Contains("Maintenence"))){
-            & $ToRun
+    try {
+        $isAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+            # $wshell = New-Object -ComObject Wscript.Shell
+        if (!($isAdmin)){
+            $Credential = New-Object System.Management.Automation.PSCredential "$((Get-ComputerInfo).CsDNSHostName)\Maintenence", (Get-SecCredentials "$env:ProgramData\OTS\data\1.dat")
+            # if (!($Credential.UserName.Contains("Maintenence"))){
+                return (Start-Process "Powershell" -Credential $Credential -ArgumentList -PassThru "Start-Process 'Powershell' -Verb RunAs -ArgumentList $ToRun")
+            # }
+            # return $null # shouldn't be here
         }
-        return $false
+        return (Start-Process 'Powershell' -Verb RunAs -PassThru -ArgumentList $ToRun)
     }
-    return $true
+    catch{
+        throw "Not able to run with admin credentials : $_"
+    }
 }
--- OTS-PSModules/Public/OTS-OfficeRepair.ps1
+++ OTS-PSModules/Public/OTS-OfficeRepair.ps1
@@ -1,8 +1,8 @@
 function OTS-OfficeRepair{
 
-    if (!(Admin-Check OTS-OfficeRepair)) {
-        return
-    } # if we are not admin then rerun with correct credentials
+    # if (!(Admin-Check OTS-OfficeRepair)) {
+    #     return
+    # } # if we are not admin then rerun with correct credentials
 
     $wshell = New-Object -ComObject Wscript.Shell
     $response = $wshell.Popup("This Script will update/repair your Office instalation. It is intended to be run in the case Office applications will not start.`n `n Please save any open work and click OK. ",0,"Office Repair",0x1)
@@ -29,30 +29,39 @@ function OTS-OfficeRepair{
         if ([long]($targetBuild | Remove-FromString -toRemove '.') -gt [long]($currentVersion | Remove-FromString -toRemove '.')){
             $actionString = @("update","updated")
             Write-Output "Starting Update to $targetBuild..."
-            Start-Process -FilePath $c2rPath -ArgumentList "/update user updatetoversion=$($targetBuild)"
+            $process = Start-Process -FilePath $c2rPath -ArgumentList "/update user updatetoversion=$($targetBuild)" -PassThru
         }
         else {
             $actionString = @("repair","repaired")
             Write-Output "Starting Repair..."
-            Start-Process -FilePath $c2rPath -Verb RunAs -ArgumentList "scenario=Repair platform=x64 culture=en-us forceappshutdown=True RepairType=FullRepair DisplayLevel=True"
+            $process = (Admin-Check "Start-Process -FilePath $c2rPath -Verb RunAs -ArgumentList 'scenario=Repair platform=x64 culture=en-us forceappshutdown=True RepairType=FullRepair DisplayLevel=True'")
         }
-        $process = Get-Process OfficeClickToRun -ErrorAction SilentlyContinue -ErrorVariable ev
-        if ($ev -ne $null) {
-            [double]$timer = 0
-            while ($err -ne $null){
-                $ev = $null
-                start-sleep -Seconds 0.2
-                $timer += 0.2
-                $process = Get-Process OfficeClickToRun -ErrorAction SilentlyContinue -ErrorVariable ev
-                if ($timer -gt 3) {throw "We missed Click-to-Run Repair, did it run?"}
-            }
-        }
-        do{ # process can restart so when it terminates we give it a sec to make sure ist really finished
-            $ev = $null
+        if ($process -ne $null){
             Wait-Process -Id $process.Id
-            start-sleep -Seconds 1
-            $process = Get-Process OfficeClickToRun -ErrorAction SilentlyContinue -ErrorVariable ev
-        } while ($ev = $null)
+        }
+        else {
+            throw "We missed Click-to-Run Repair, did it run?"
+        }
+
+        #old wait process code
+        # $process = Get-Process OfficeClickToRun -ErrorAction SilentlyContinue -ErrorVariable ev
+        # if ($ev -ne $null) {
+        #     [double]$timer = 0
+        #     while ($err -ne $null){
+        #         $ev = $null
+        #         start-sleep -Seconds 0.2
+        #         $timer += 0.2
+        #         $process = Get-Process OfficeClickToRun -ErrorAction SilentlyContinue -ErrorVariable ev
+        #         if ($timer -gt 3) {throw "We missed Click-to-Run Repair, did it run?"}
+        #     }
+        # }
+        # do{ # process can restart so when it terminates we give it a sec to make sure ist really finished
+        #     $ev = $null
+        #     Wait-Process -Id $process.Id
+        #     start-sleep -Seconds 1
+        #     $process = Get-Process OfficeClickToRun -ErrorAction SilentlyContinue -ErrorVariable ev
+        # } while ($ev = $null)
+
      }
     catch {
         $response = $wshell.Popup("The $($actionString[0]) did not complete succesfully. `n `nPlease contact Olympus service desk for help at service_desk@OlympusTech.com.au, on 1800 932 964, or by right clicking on the Olympus icon in the taskbar and selecting 'Create Ticket'. `n`nError: $_",0,"Failed",	0x30)
