9b343574d73fa3f340c5fa83551c84087f0cc4b7
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-09 21:48:11 +1100

    hopefully now should run with admin credentials

--- OTS-PSModules/Private/Admin-Check.ps1
+++ OTS-PSModules/Private/Admin-Check.ps1
@@ -1,16 +1,20 @@
 function Admin-Check{
     param(
         [string]$Process,
-        [string]$ArgList
+        [string]$ArgList,
+        [int]$Count
     )
     try {
         $isAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
             # $wshell = New-Object -ComObject Wscript.Shell
         if (!($isAdmin)){
             $Credential = New-Object System.Management.Automation.PSCredential "$((Get-ComputerInfo).CsDNSHostName)\Maintenence", (Get-SecCredentials "$env:ProgramData\OTS\data\1.dat")
-            # if (!($Credential.UserName.Contains("Maintenence"))){
-                Start-Process "Powershell" -Credential $Credential -Wait -ArgumentList "Start-Process $process -Verb RunAs -Wait -ArgumentList $ArgList"
-            # }
+            if (!($Credential.UserName.Contains("Maintenence"))){
+                Start-Process "Powershell" -Credential $Credential -Wait -ArgumentList "Admin-Check -Process $Process -ArgList $ArgList -Count $($Count+1)"
+             }
+             else {
+                Start-Process "Powershell" -Verb RunAs -Wait -ArgumentList "Admin-Check -Process $Process -ArgList $ArgList -Count $($Count+1)"
+             }
             # return $null # shouldn't be here
         }
         else {
