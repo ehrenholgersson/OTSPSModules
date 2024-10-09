6151350c4b24bdbaf6352e56e1630a4949f9753b
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-09 18:14:45 +1100

    added credentials to repair

--- OTS-PSModules/Public/OTS-OfficeRepair.ps1
+++ OTS-PSModules/Public/OTS-OfficeRepair.ps1
@@ -29,7 +29,8 @@ function OTS-OfficeRepair{
         else {
             $actionString = @("repair","repaired")
             Write-Output "Starting Repair..."
-            Start-Process -FilePath $c2rPath -ArgumentList "scenario=Repair platform=x64 culture=en-us forceappshutdown=True RepairType=FullRepair DisplayLevel=True"
+            $Credential = New-Object System.Management.Automation.PSCredential "$((Get-ComputerInfo).CsDNSHostName)\Maintenence" Get-SecCredentials "$env:ProgramData\OTS\data\1.dat"
+            Start-Process -FilePath $c2rPath -Credential $Credential -ArgumentList "scenario=Repair platform=x64 culture=en-us forceappshutdown=True RepairType=FullRepair DisplayLevel=True"
         }
         $process = Get-Process OfficeClickToRun -ErrorAction SilentlyContinue -ErrorVariable ev
         if ($ev -ne $null) {
