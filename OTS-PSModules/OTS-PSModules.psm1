cdc760a3055849f547b638adee8441aba69feda3
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-15 14:02:23 +1100

    key now generated by cmdlet in dll

--- OTS-PSModules/OTS-PSModules.psm1
+++ OTS-PSModules/OTS-PSModules.psm1
@@ -1,6 +1,9 @@
 foreach ($script in (Get-ChildItem -Path $PSScriptRoot -Depth 3 | ? {$_.FullName.Contains(".ps1")})){
     . $script.FullName
 }
+foreach ($dll in (Get-ChildItem -Path $PSScriptRoot -Depth 3 | ? {$_.FullName.Contains(".dll") -and ($_.FullName.Contains("Public") -or $_.FullName.Contains("Private"))})){
+    Import-Module  $dll.Name
+}
 foreach ($script in (Get-ChildItem -Path $PSScriptRoot -Depth 3 | ? {$_.FullName.Contains(".ps1") -and $_.FullName.Contains("Public")})){
     Export-ModuleMember  $script.Name.trim(".ps1")
 }
--- OTS-PSModules/Private/Get-SecCredentials.ps1
+++ OTS-PSModules/Private/Get-SecCredentials.ps1
@@ -1,14 +1,11 @@
 function Get-SecCredentials{
+
     param(
         [Parameter(Mandatory)][string]$Path
     )
 
-    $datetime = ([DateTimeOffset](Get-Date (Get-Item -Path $Path).CreationTime -Format d)).ToUnixTimeSeconds()
-
-    [byte[]]$timeKey = @(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-    for ($i = 0;$i -lt 8; $i++){
-        $timeKey[$i] = ($datetime -shr (8*$i)) -band 255
-    }
+    $timeKey = Get-UnlockKey($Path)
     $result = (Get-Content -Path $path) | ConvertTo-SecureString -Key $timeKey
     return $result
+
 }
