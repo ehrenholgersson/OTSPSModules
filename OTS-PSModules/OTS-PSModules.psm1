f6d760bb134b0578f85f1c6a1b0996d2a697d4a3
Ehren@Work <ehren.holgersson@olympustech.com.au>
2024-10-15 14:39:49 +1100

    added dll import

--- OTS-PSModules/OTS-PSModules.psm1
+++ OTS-PSModules/OTS-PSModules.psm1
@@ -2,7 +2,7 @@ foreach ($script in (Get-ChildItem -Path $PSScriptRoot -Depth 3 | ? {$_.FullName
     . $script.FullName
 }
 foreach ($dll in (Get-ChildItem -Path $PSScriptRoot -Depth 3 | ? {$_.FullName.Contains(".dll") -and ($_.FullName.Contains("Public") -or $_.FullName.Contains("Private"))})){
-    Import-Module  $dll.Name
+    Import-Module  $dll.FullName
 }
 foreach ($script in (Get-ChildItem -Path $PSScriptRoot -Depth 3 | ? {$_.FullName.Contains(".ps1") -and $_.FullName.Contains("Public")})){
     Export-ModuleMember  $script.Name.trim(".ps1")
